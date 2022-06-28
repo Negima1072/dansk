390169418d50cf0de58314901bb802f331eb43b9
xpa <xpadev@gmail.com>
2022-06-28 13:30:13 +0900

    convert embed.ts to typescript

--- src/@types/types.d.ts
+++ src/@types/types.d.ts
@@ -1,5 +1,20 @@
 type ownerComment = {
-	"time": string,
-	"command": string,
-	"comment": string
-}
+  time: string;
+  command: string;
+  comment: string;
+};
+type messageColorClickEvent = {
+  type: "color_click";
+};
+type messageTimeSeekEvent = {
+  type: "time_seek";
+  text: string;
+};
+type messageTimeSeekIntEvent = {
+  type: "time_seek_int";
+  int: number;
+};
+type messageTimeSeekPlEvent = {
+  type: "time_seek_pl";
+  pl: string[];
+};
--- src/typeGuard.ts
+++ src/typeGuard.ts
@@ -10,6 +10,24 @@ const typeGuard = {
       return true;
     },
   },
+  messageEvent: {
+    colorClick: (i: unknown): i is messageColorClickEvent =>
+      typeof i === "object" &&
+      i !== null &&
+      (i as messageColorClickEvent).type === "color_click",
+    timeSeek: (i: unknown): i is messageTimeSeekEvent =>
+      typeof i === "object" &&
+      i !== null &&
+      (i as messageTimeSeekEvent).type === "time_seek",
+    timeSeekInt: (i: unknown): i is messageTimeSeekIntEvent =>
+      typeof i === "object" &&
+      i !== null &&
+      (i as messageTimeSeekIntEvent).type === "time_seek_int",
+    timeSeekPl: (i: unknown): i is messageTimeSeekPlEvent =>
+      typeof i === "object" &&
+      i !== null &&
+      (i as messageTimeSeekPlEvent).type === "time_seek_pl",
+  },
 };
 const typeVerify = (item: unknown, keys: string[]): boolean => {
   if (typeof item !== "object" || item === null) return false;
