7325a03320739940142c17d3282eb219efbccc41
xpa <xpadev@gmail.com>
2022-07-22 17:17:47 +0900

    rename json to ts and add type data

---
+++ src/libraries/layerUtil.charList.ts
@@ -0,0 +1,81 @@
+import { CharList } from "@/@types/types";
+
+const CharList: CharList = {
+  "\u2003": {
+    width: 12,
+    isSpace: true,
+  },
+  "\u2002": {
+    width: 6,
+    isSpace: true,
+  },
+  "\u2004": {
+    width: 4,
+    isSpace: true,
+  },
+  "\u2005": {
+    width: 3,
+    isSpace: true,
+  },
+  "\u2006": {
+    width: 2,
+    isSpace: true,
+  },
+  "\u200A": {
+    width: 1,
+    isSpace: true,
+  },
+  "\u005F": {
+    width: 6,
+    isSpace: false,
+  },
+  "\uFF70": {
+    width: 6,
+    isSpace: false,
+  },
+  "\u00AF": {
+    width: 6,
+    isSpace: false,
+  },
+  "\u2216": {
+    width: 6,
+    isSpace: false,
+  },
+  "\uFFE8": {
+    width: 6,
+    isSpace: false,
+  },
+  "\u2580": {
+    width: 8.5,
+    isSpace: false,
+  },
+  "\u2590": {
+    width: 8.5,
+    isSpace: false,
+  },
+  "\u2043": {
+    width: 3.692307,
+    isSpace: false,
+  },
+  "\u01C0": {
+    width: 3.111111,
+    isSpace: false,
+  },
+  "\u207B": {
+    width: 3.466666,
+    isSpace: false,
+  },
+  "\u208B": {
+    width: 3.466666,
+    isSpace: false,
+  },
+  "\u002F": {
+    width: 5.647058,
+    isSpace: false,
+  },
+  default: {
+    width: 12,
+    isSpace: false,
+  },
+};
+export default CharList;
