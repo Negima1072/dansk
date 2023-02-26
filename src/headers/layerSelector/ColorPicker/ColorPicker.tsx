c7644aeebb1163b6c718b11f33d83313d073c5a3
xpa <xpadev@gmail.com>
2023-02-27 02:15:15 +0900

    fix: input value warning on chrome

--- src/headers/layerSelector/ColorPicker/ColorPicker.tsx
+++ src/headers/layerSelector/ColorPicker/ColorPicker.tsx
@@ -62,13 +62,15 @@ const ColorPicker = ({ color, disabled, onChange }: props) => {
         ref={colorDisplayRef}
         onMouseEnter={onClick}
       >
-        <input
-          className={Styles.colorInput}
-          type="color"
-          onChange={(e) => onChange(e)}
-          disabled={disabled}
-          value={colorInput}
-        />
+        {!disabled && (
+          <input
+            className={Styles.colorInput}
+            type="color"
+            onChange={(e) => onChange(e)}
+            disabled={disabled}
+            value={colorInput}
+          />
+        )}
       </ColorDisplay>
       <HoverItem x={pos.x} y={pos.y + pos.height} className={Styles.hoverItem}>
         <input
