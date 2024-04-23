1c7c65a22189093b491698f77542078098bfede2
xpadev <xpadev@gmail.com>
2024-04-23 13:26:51 +0900

    chore: add test to lint-staged hook

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,3 +1,4 @@
 export default {
 	"src/**/*.{ts,tsx,json,scss,css}": [()=>"npm run eslint:fix",()=>"npm run check-types",()=>"npm run format"],
+	"src/libraries/layerUtil/*": [()=>"npm run test"]
 };
