3144c3db7bb3c081d126f496bc227002c6dc8c86
Negima1072 <ngm1072@gmail.com>
2024-12-30 17:30:24 +0900

    fix: github action

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,13 +1,23 @@
-import { defineConfig } from "wxt";
+import { UserManifest, defineConfig } from "wxt";
 
 export default defineConfig({
   srcDir: "src",
   publicDir: "../public",
   extensionApi: "chrome",
   modules: ["@wxt-dev/module-react"],
-  manifest: {
-    name: "段スク水",
-    description: "Modified By @Negima1072, @eneko0513 and @xpadev-net",
+  manifest: (env) => {
+    const manifest: UserManifest = {
+      name: "段スク水",
+      description: "ニコニコ動画のコメントアート制作を補助するブラウザ拡張機能",
+    };
+    if (env.browser === "firefox") {
+      manifest.browser_specific_settings = {
+        gecko: {
+          id: "{781bb7bf-090f-df5d-4976-67b3b958c4fe}",
+        },
+      };
+    }
+    return manifest;
   },
   vite: () => ({
     css: {
